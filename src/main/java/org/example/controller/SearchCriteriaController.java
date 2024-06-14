79a525210944f1f18f7f60602ba264132366a284
Laurentiu Cimpan <I478560@fhict.nl>
2024-06-14 17:10:48 +0200

    Search criteria controller updated for creating and updating all 3 types of criteria(category, brand and model)

--- src/main/java/org/example/controller/SearchCriteriaController.java
+++ src/main/java/org/example/controller/SearchCriteriaController.java
@@ -1,18 +1,15 @@
 package org.example.controller;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.example.business.dto.carBrandDTO.GetAllCarBrandsRequest;
-import org.example.business.dto.carBrandDTO.GetAllCarBrandsResponse;
-import org.example.business.dto.carModelDTO.GetCarModelByCarBrand;
-import org.example.business.dto.carModelDTO.GetCarModelByCarBrandResponse;
-import org.example.business.dto.categoryDTO.GetAllCategoriesRequest;
-import org.example.business.dto.categoryDTO.GetAllCategoriesResponse;
-import org.example.business.searchCriteria.GetAllCarBrandsUseCase;
-import org.example.business.searchCriteria.GetAllCategoriesUseCase;
-import org.example.business.searchCriteria.GetCarModelsUseCase;
+import org.example.business.dto.carBrandDTO.*;
+import org.example.business.dto.carModelDTO.*;
+import org.example.business.dto.categoryDTO.*;
+import org.example.business.searchCriteria.*;
 import org.example.persistance.CarBrandRepository;
 import org.example.persistance.CarModelRepository;
 import org.example.persistance.CategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,8 +24,54 @@ public class SearchCriteriaController {
     private final GetAllCategoriesUseCase getAllCategoriesUseCase;
     private final GetAllCarBrandsUseCase getAllCarBrandsUseCase;
     private final GetCarModelsUseCase getCarModelsUseCase;
+    private final CreateCategoryUseCase createCategoryUseCase;
+    private final CreateCarBrandUseCase createCarBrandUseCase;
+    private final CreateCarModelUseCase createCarModelUseCase;
+    private  final UpdateCategoryUseCase updateCategoryUseCase;
+    private final UpdateCarModelUseCase updateCarModelUseCase;
+    private final UpdateCarBrandUseCase updateCarBrandUseCase;
 
     //test for pipeline
+    @PostMapping("/categoryCreate")
+    public ResponseEntity<CreateCategoryResponse> createCategory(@RequestBody @Valid CreateCategoryRequest request){
+        CreateCategoryResponse response = createCategoryUseCase.createCategory(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/brandCreate")
+    public ResponseEntity<CreateCarBrandResponse> createCarbrand(@RequestBody @Valid CreateCarBrandRequest request){
+        CreateCarBrandResponse response = createCarBrandUseCase.createCarBrand(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/modelCreate")
+    public ResponseEntity<CreateCarModelResponse> createCarModel(@RequestBody @Valid CreateCarModelRequest request){
+        CreateCarModelResponse response = createCarModelUseCase.createCarModel(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("{categoryID}")
+    public ResponseEntity<Void> updateCategory(@PathVariable("categoryID") long categoryID, @RequestBody @Valid UpdateCategoryRequest request){
+        request.setCategoryID(categoryID);
+        updateCategoryUseCase.updateCategory(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{brandID}")
+    public ResponseEntity<Void> updateBrand(@PathVariable("brandID") long brandID, @RequestBody @Valid UpdateCarBrandRequest request){
+        request.setBrandID(brandID);
+        updateCarBrandUseCase.updateCarBrand(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{modelID}")
+    public ResponseEntity<Void> updateModel(@PathVariable("modelID") long modelID, @RequestBody @Valid UpdateCarModelRequest request){
+        request.setModelID((modelID));
+        updateCarModelUseCase.updateCarModel(request);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
     @GetMapping("/category")
     public ResponseEntity<GetAllCategoriesResponse> getAllCategories(){
